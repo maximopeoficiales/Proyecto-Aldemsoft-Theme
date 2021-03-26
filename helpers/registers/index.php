4d55faa36050fd1f85fa28e20c0a2ffce1828fb1
Maximo Junior Apaza Chirhuana <maximopeoficiales@gmail.com>
2021-03-26 14:30:26 -0500

    falta agregar query ubigeo

--- helpers/registers/index.php
+++ helpers/registers/index.php
@@ -7,7 +7,10 @@
 function aldem_head_js()
 {
     if (aldem_verify_exists_shortcodes()) {
+        // libreria necesarias para boostrap sin jquery porque el tema ya lo tiene
         wp_enqueue_style("boostrapCustomMin", "https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css", '', '1.0.0');
+        
+        wp_enqueue_script("boostrapMinJS", "https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.bundle.js", '', '1.0.0', true);
 
         // datatables
         wp_enqueue_style("aldemDatatablesCSS", "https://cdn.datatables.net/1.10.24/css/jquery.dataTables.min.css", '', '1.0.0');
@@ -17,8 +20,8 @@ function aldem_head_js()
         wp_enqueue_script("aldemDatatablesJS", "https://cdn.datatables.net/1.10.24/js/jquery.dataTables.min.js", '', '1.0.0', true);
         wp_enqueue_script("aldemDatatablesB4-JS", "https://cdn.datatables.net/1.10.24/js/dataTables.bootstrap4.min.js", '', '1.0.0', true);
         wp_enqueue_script("aldemDatatablesResposiveJS", "https://cdn.datatables.net/responsive/2.2.7/js/dataTables.responsive.min.js", '', '1.0.0', true);
-        wp_enqueue_script("aldemDatatablesB4-ResposiveJS", "https://cdn.datatables.net/responsive/2.2.7/js/responsive.bootstrap4.min.js
-    ", '', '1.0.0', true);
+        //     wp_enqueue_script("aldemDatatablesB4-ResposiveJS", "https://cdn.datatables.net/responsive/2.2.7/js/responsive.bootstrap4.min.js
+        // ", '', '1.0.0', true);
         // FIN DE DATATABLES
 
         // registro css global para todo el tema
--- helpers/views/shippers.php
+++ helpers/views/shippers.php
@@ -1,6 +1,8 @@
 <?php
 
 $shippers = (object) query_getShippers();
+$countrys = (object) query_getCountrys();
+// print_r(query_getUbigeo());
 
 ?>
 
@@ -20,36 +22,97 @@ $shippers = (object) query_getShippers();
                         </tr>
                     </thead>
                     <tbody>
-                        <?php foreach ($shippers as $shipper) {
+                        <?php foreach ($shippers as $key => $shipper) {
                         ?>
                             <!-- Modal -->
                             <tr>
-                                <td><?= $shipper->nombre ?></td>
+                                <td class="d-flex justify-content-between" style="align-items: center !important;">
+                                    <span><?= $shipper->nombre ?></span>
+                                    <button type="button" class="btn" data-toggle="modal" data-target="#exampleModal<?= $key + 1 ?>"><i class="fa fa-eye" aria-hidden="true"></i></button>
+                                </td>
                                 <td><?= $shipper->direccion ?></td>
                                 <td><?= $shipper->site ?></td>
+
                             </tr>
                         <?php }  ?>
                     </tbody>
                 </table>
-                <div class="modal" id="exampleModal<?= $shipper->id_shipper  ?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true" style="margin-top: 100px;">
-                    <div class="modal-dialog" role="document">
-                        <div class="modal-content">
-                            <div class="modal-header">
-                                <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                    <span aria-hidden="true">&times;</span>
-                                </button>
-                            </div>
-                            <div class="modal-body">
-                                ...
-                            </div>
-                            <div class="modal-footer">
-                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                                <button type="button" class="btn btn-primary">Save changes</button>
+                <?php foreach ($shippers as $key1 => $ship) { ?>
+                    <div class="modal" id="exampleModal<?= $key1 + 1  ?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true" style="margin-top: 100px;">
+                        <div class="modal-dialog" role="document">
+                            <div class="modal-content">
+                                <div class="modal-header bg-dark text-white">
+                                    <h5 class="modal-title" id="exampleModalLabel">Detalle de Shipper</h5>
+                                    <button type="button" class="close text-white" data-dismiss="modal" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                    </button>
+                                </div>
+                                <div class="modal-body">
+                                    <div class="form-group">
+                                        <label for="nombreShipper">Nombre: </label>
+                                        <input type="text" name="nombreShipper" id="nombreShipper" class="form-control" placeholder="Ingrese su nombre" aria-describedby="helpId" value="<?= $ship->nombre ?>">
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="direccionShipper">Direccion:</label>
+                                        <input type="text" name="direccionShipper" id="direccionShipper" class="form-control" placeholder="Ingrese su direccion" aria-describedby="helpId" value="<?= $ship->direccion ?>">
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="direccion2Shipper">Direccion2:</label>
+                                        <input type="text" name="direccion2Shipper" id="direccion2Shipper" class="form-control" placeholder="Ingrese su direccion2" aria-describedby="helpId" value="<?= $ship->direccion2 ?>">
+                                    </div>
+                                    <div class="row mt-2">
+                                        <div class="col-md-6">
+                                            <div class="form-group">
+                                                <label for="zipShipper">Zip:</label>
+                                                <input type="number" name="zipShipper" id="zipShipper" class="form-control" placeholder="Ingrese su zip" aria-describedby="helpId" min="0" value="<?= $ship->zip ?>">
+                                            </div>
+                                        </div>
+                                        <div class="col-md-6">
+                                            <div class="form-group">
+                                                <label for="paisShipper">Pais:</label>
+                                                <select class="form-control" name="paisShipper" id="paisShipper">
+                                                    <option selected>FASDF</option>
+                                                    <option>ASDF</option>
+                                                    <option>ASF</option>
+                                                </select>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="row mt-2">
+                                        <div class="col-md-6">
+                                            <div class="form-group">
+                                                <label for="shiteShipper">Site:</label>
+                                                <select class="form-control" name="shiteShipper" id="shiteShipper">
+                                                    <option>FASDF</option>
+                                                    <option>ASDF</option>
+                                                    <option>ASF</option>
+                                                </select>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-6">
+                                            <div class="form-group ">
+                                                <label for="ubigeoShipper">Ubigeo:</label>
+                                                <select class="form-control" name="ubigeoShipper" id="ubigeoShipper">
+                                                    <option>FASDF</option>
+                                                    <option>ASDF</option>
+                                                    <option>ASF</option>
+                                                </select>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <p class="mt-2">
+                                        <strong>Creador por: </strong> lkjaslkjdfk
+                                    </p>
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-danger text-capitalize" data-dismiss="modal">Salir</button>
+                                    <button type="button" class="btn btn-success text-capitalize">Guardar Cambios</button>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
+                <?php } ?>
                 <div class="text-center">
                     <button class="btn">Nuevo shipper</button>
                 </div>
